5dee5daad4f16828ab6bff8c7d12c5ba07eead8b
Lykeen Morales <morales.lykeen@ici.edu.ph>
2024-10-05 14:57:06 +0000

    changes

--- htdocs/PT5/mainpage.php
+++ htdocs/PT5/mainpage.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="mainDesign.css">
+
 </head>
+
 <body>
     <div class="MainTable">
-        <table border = "1">
+        <table>
             <tr>
                 <th>test</th>
             </tr>
