86e839a1632cd209bfc97fee3d489c3baaf44b38
Lykeen Morales <morales.lykeen@ici.edu.ph>
2024-10-06 13:59:00 +0000

    New Files/ Changes

---
+++ htdocs/PT5/Pages/EditPage/productEdit.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../../mainDesign.css">
+
+</head>
+
+<body>
+    <h2>Customers Information</h2>
+    <table>
+        <tr>
+            <td>First Name:</td>
+            <td><input type="text" name="First Name" placeholder="Enter your first name"></td>
+        </tr>
+        <tr>
+            <td>Last Name</td>
+            <td><input type="text" name="Last Name", placeholder="Enter your last name"></td>
+        </tr> 
+        <tr>
+            <td>Phone Number</td>
+            <td><input type="text" name="Last Name", placeholder="Enter your Phone Number"></td>
+        </tr>  
+        <tr>
+            <td>Last Name</td>
+            <td><input type="text" name="Address", placeholder="Enter your Address"></td>
+        </tr>   
+    </table>
+</body>
+</html>
---
+++ htdocs/PT5/Pages/GetData.php
@@ -0,0 +1,34 @@
+<?php
+    include '../connection.php';
+    include '../config.php';
+
+    // We are just returning the Query from the database
+     function getDataTable($Callback, $Query_Conn){
+        if (gettype($Callback) != "string"){
+            return "Invalid Input";
+        }
+
+        if ($Callback == "GetProductInformation"){
+            $QUERY = "SELECT * From products";
+            $QueryResult = mysqli_query($Query_Conn, $QUERY);
+
+            // Check if Result Variable is Valid/ Not Empty
+            if (!$QueryResult){
+                return "Error While Fetching Data";
+            }
+            return $QueryResult;
+        }elseif($Callback == "GetCustomerInformation"){
+            $QUERY = "SELECT * From customeraccount";
+            $QueryResult = mysqli_query($Query_Conn, $QUERY);
+
+            // Check if Result Variable is Valid/ Not Empty
+            if (!$QueryResult){
+                return "Error While Fetching Data";
+            }
+            return $QueryResult;
+        }else{
+            return "Cant find" . $Callback;
+        }
+    }
+    
+?>
